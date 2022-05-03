7b17cb8b727cf7215c18d320422421fc560dfe0f
Amir M. Mir <mir-am@hotmail.com>
2022-05-03 13:57:15 +0200

    Correct Kafka bootstrap addresses

--- src/main/scala/org/codefeedr/pypi/Main.scala
+++ src/main/scala/org/codefeedr/pypi/Main.scala
@@ -26,8 +26,8 @@ object Main {
       .setBufferProperty(KafkaBuffer.COMPRESSION_TYPE, "gzip")
       .setBufferProperty(KafkaBuffer.AMOUNT_OF_PARTITIONS, "8")
       .setBufferProperty(KafkaBuffer.AMOUNT_OF_REPLICAS, "2")
-      .setBufferProperty(KafkaBuffer.BROKER, "kafka-0.kafka-headless.codefeedr:9092,kafka-1.kafka-headless.codefeedr:9092,kafka-2.kafka-headless.codefeedr:9092")
-      .setBufferProperty(KafkaBuffer.ZOOKEEPER, "zookeeper-0.zookeeper-headless.codefeedr:2181,zookeeper-1.zookeeper-headless.codefeedr:2181,zookeeper-2.zookeeper-headless.codefeedr:2181")
+      .setBufferProperty(KafkaBuffer.BROKER, "131.180.178.92:9092,131.180.178.90:9092")
+      .setBufferProperty(KafkaBuffer.ZOOKEEPER, "131.180.178.92:2181,131.180.178.90:2181")
       .setBufferProperty("message.max.bytes", "10485760") // max message size is 10mb
       .setBufferProperty("max.request.size", "10485760") // max message size is 10 mb
       .edge(releaseSource, enrichReleases)
